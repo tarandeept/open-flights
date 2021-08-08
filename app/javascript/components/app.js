c9cd03f1817a39ddc6e5b5c553c0aa72a63c111b
Tarandeep Singh Thamrait <tthamrai@uci.edu>
2021-08-08 11:26:49 -0700

    Wrote api

---
+++ app/javascript/components/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Hello World
+      Hello World
+      Hello World
+      Hello World
+      Hello World
+      Hello World
+    </div>
+  )
+}
+
+export default App
