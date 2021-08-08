bcc996d39fd8e1c63f8581b72ec13b08ae912f58
Tarandeep Singh Thamrait <tthamrai@uci.edu>
2021-08-08 11:45:57 -0700

    Rendering airlines

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {
+    // Get all airlines from API
+    // Update airlines in out state
+
+    axios.get('/api/v1/airlines')
+    .then(res => {
+      setAirlines(res.data.data)
+    })
+    .catch(res => console.log(res ))
+  }, [airlines.length])
+
+  const list = airlines.map(item => {
+    return (<li key={item.id}>{item.attributes.name}</li>)
+  })
+
   return (
-    <h1>This is the Airlines#index view for our app.</h1>
+    <div className="home">
+      <div className="header">
+        <h1>OpenFlights</h1>
+        <div className="subheader">Honest, unbiased, airline reviews</div>
+      </div>
+      <div className="grid">
+        <ul>{list}</ul>
+      </div>
+    </div>
   )
 }
 
