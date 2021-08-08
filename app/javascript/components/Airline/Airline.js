48eb225a0f5f532a4cff7afe0d3f620907218e7b
Tarandeep Singh Thamrait <tthamrai@uci.edu>
2021-08-08 11:34:34 -0700

    Setting up react components

---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Airline = () => {
+  return (
+    <h1>This is the Airlines#show view for our app.</h1>
+  )
+}
+
+export default Airline
---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Airlines = () => {
+  return (
+    <h1>This is the Airlines#index view for our app.</h1>
+  )
+}
+
+export default Airlines
